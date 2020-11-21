4b850097201ed5917ce46c50bdf24d01d154dbf7
Edward Leung <edward_leung_12@hotmail.com>
2020-11-21 00:13:12 +0000

    added an overview of the movie when user hover over

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function showMovies(movies) {
   main.innerHTML = '';
 
   movies.forEach((movie) => {
-    const {poster_path, title, vote_average} = movie;
+    const {poster_path, title, vote_average, overview} = movie;
     const movieEl = document.createElement('div');
     movieEl.classList.add('movie');
 
@@ -34,6 +34,10 @@ function showMovies(movies) {
       <div class="movie-info">
         <h3>${title}</h3>
         <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      </div>
+      <div class="overview">
+        <h4>Overview:</h4>
+        ${overview}
       </div>
     `;
 
